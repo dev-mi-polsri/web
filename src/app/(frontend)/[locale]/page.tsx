d7ddca579232e127c7cd860c2910af593fe22c8e
notilmi <dznilmi@gmail.com>
2025-05-05 21:18:40 +0700

    feat: Refactor Partners component to use server-side data fetching and pass locale prop from HomePage

--- src/app/(frontend)/[locale]/page.tsx
+++ src/app/(frontend)/[locale]/page.tsx
@@ -25,7 +25,7 @@ export default async function HomePage({ params }: { params: Promise<{ locale: s
   return (
     <div className="flex flex-col">
       <Hero locale={locale} />
-      <Partners />
+      <Partners locale={locale} />
       <Profile />
       <Facilities />
       <Stats />
