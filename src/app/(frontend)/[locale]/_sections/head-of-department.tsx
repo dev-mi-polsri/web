eab1a3f183a658b31169afbd35258333e4eb5391
RAFIUSER <rafiussyakban@gmail.com>
2025-05-05 21:30:54 +0700

    kata sambutan kajur section

--- src/app/(frontend)/[locale]/_sections/head-of-department.tsx
+++ src/app/(frontend)/[locale]/_sections/head-of-department.tsx
@@ -5,13 +5,15 @@ import Image from 'next/image'
 
 const HeadOfDepartment = () => {
   return (
-    <section className="py-8">
-      <div className="max-w-7xl mx-auto flex flex-col lg:flex-row justify-between gap-4 md:p-4 p-6">
-        {/* Quote  */}
-        <div className="lg:w-1/2 flex gap-4 flex-col">
-          <h1 className="font-bold text-2xl">Kata Sambutan Kepala Jurusan Manajemen Informatika</h1>
+    <section className="py-12 bg-gray-50">
+      <div className="max-w-7xl mx-auto flex flex-col lg:flex-row justify-between gap-8 px-6 lg:px-8">
+        {/* Quote Section */}
+        <div className="lg:w-1/2 flex flex-col gap-6">
+          <h1 className="font-extrabold text-2xl text-gray-800">
+            Kata Sambutan Kepala Jurusan Manajemen Informatika
+          </h1>
           <p className="text-muted-foreground text-lg text-justify">
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Culpa dignissimos ad, iure
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa dignissimos ad, iure
             doloribus dolor ullam eius, accusantium illum, consequuntur obcaecati nihil nobis
             repudiandae cupiditate. Dolor, reprehenderit unde consequuntur minima voluptates nisi
             reiciendis illo nihil expedita quidem suscipit error doloremque explicabo dignissimos
@@ -19,13 +21,13 @@ const HeadOfDepartment = () => {
           </p>
         </div>
 
-        {/* Image */}
-        <div className="w-sm lg:w-md">
-          <Card className="overflow-hidden w-full h-full object-cover rounded-2xl shadow-lg py-0">
+        {/* Image Section */}
+        <div className="w-full lg:w-auto flex justify-center">
+          <Card className="overflow-hidden w-full max-w-sm rounded-2xl shadow-lg py-0">
             <Image
               key="Head-3.jpg"
               src="/sony-oktapriandi.JPG"
-              alt="headofdepartment"
+              alt="Head of Department"
               width={1080}
               height={1080}
               className="w-full h-full object-cover"
