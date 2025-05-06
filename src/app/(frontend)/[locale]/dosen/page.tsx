08ed95b82c4094b98e25803f36c115c89e316ffc
notilmi <dznilmi@gmail.com>
2025-05-06 11:29:26 +0700

    feat: Add lecturers section with dynamic title in English and Indonesian translations

--- src/app/(frontend)/[locale]/dosen/page.tsx
+++ src/app/(frontend)/[locale]/dosen/page.tsx
@@ -1,11 +1,16 @@
-'use client'
 import React from 'react'
 import DosenList from './list'
+import { getMessages } from 'next-intl/server'
 
-function Page() {
+async function Page({ params }: { params: Promise<{ locale: string }> }) {
+  const { locale } = await params
+
+  const {
+    pages: { lecturers: t },
+  } = await getMessages({ locale })
   return (
     <div className="flex flex-col gap-4 items-center py-24 max-w-screen-lg mx-auto">
-      <h1 className="text-2xl font-bold mb-6">Daftar Dosen</h1>
+      <h1 className="text-2xl font-bold mb-6">{t.title}</h1>
       <DosenList />
     </div>
   )
