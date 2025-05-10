7bf8c82e08d46f6933595ef7f9f2f2a765050fe0
notilmi <dznilmi@gmail.com>
2025-05-10 12:54:16 +0700

    feat: add @langchain/openai dependency and adjust temperature setting in ChatGoogleGenerativeAI

--- src/app/api/ai/route.ts
+++ src/app/api/ai/route.ts
@@ -68,7 +68,7 @@ export async function POST(req: NextRequest) {
 
     const model = new ChatGoogleGenerativeAI({
       model: 'gemini-2.0-flash',
-      temperature: 0.2,
+      temperature: 0.4,
     })
 
     const vectorstore = loadVectorStore()
