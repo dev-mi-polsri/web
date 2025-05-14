b1b74ca9d19d880d89bb644592f8671255344671
notilmi <dznilmi@gmail.com>
2025-05-14 07:11:36 +0700

    Update route.ts

--- src/app/api/ai/route.ts
+++ src/app/api/ai/route.ts
@@ -67,7 +67,7 @@ export async function POST(req: NextRequest) {
     const currentMessageContent = messages[messages.length - 1].content
 
     const model = new ChatGoogleGenerativeAI({
-      model: 'gemini-2.5-flash-preview-04-17',
+      model: 'gemini-2.0-flash',
       temperature: 0.4,
     })
 
