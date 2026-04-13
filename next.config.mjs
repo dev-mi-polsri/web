1d9f413d0133f2d9ebbf9613205f04f5d08be665
Achmad Ilmi / Terence <dznilmi@gmail.com>
2026-04-13 15:42:01 +0700

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -5,7 +5,6 @@ const withNextIntl = createNextIntlPlugin()
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // Your Next.js config here
-  output: 'standalone',
   images: {
     remotePatterns: [
       {
