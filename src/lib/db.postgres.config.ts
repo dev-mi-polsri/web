335dd701db0cb98c7a9e1c22fb8b3cf1db627c01
Achmad Ilmi / Terence <dznilmi@gmail.com>
2026-04-14 07:17:56 +0700

    impr(postgres): Use POSTGRES_URL for connection

--- src/lib/db.postgres.config.ts
+++ src/lib/db.postgres.config.ts
@@ -1,11 +1,5 @@
 import { PoolConfig } from 'pg'
 
 export const DATABASE_POSTGRES_CONFIG: PoolConfig = {
-  host: process.env.POSTGRES_HOST,
-  user: process.env.POSTGRES_USER,
-  password: process.env.POSTGRES_PASSWORD,
-  database: process.env.POSTGRES_DATABASE,
-  port: process.env.POSTGRES_PORT ? parseInt(process.env.POSTGRES_PORT) : 5432,
-  max: process.env.POSTGRES_CONNECTION_LIMIT ? parseInt(process.env.POSTGRES_CONNECTION_LIMIT) : 10,
-  // Timezone is handled at the database level via SQL SET timezone
+  connectionString: process.env.POSTGRES_URL,
 }
